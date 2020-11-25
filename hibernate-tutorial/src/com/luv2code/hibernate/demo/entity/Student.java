6063fdc9f1f93f8a983ed5e1e6588ab80ee56e7b
Rahul <raahool.deshmukh@gmail.com>
2020-11-24 19:12:35 -0600

    Added toString to Student entity to enable better logging

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/entity/Student.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/entity/Student.java
@@ -64,6 +64,10 @@ public class Student {
 		this.email = email;
 	}
 	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
 	
 	
 	
